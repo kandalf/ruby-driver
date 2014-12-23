37e32837e1f68971daab1069ef9d4f89df603ae0
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-12-23 15:20:06 -0800

    switch to 2.1.2 as the default cassandra version

--- support/ccm.rb
+++ support/ccm.rb
@@ -658,7 +658,7 @@ module CCM extend self
   end
 
   def cassandra_version
-    ENV['CASSANDRA_VERSION'] || '2.0.10'
+    ENV['CASSANDRA_VERSION'] || '2.1.2'
   end
 
   def setup_cluster(no_dc = 1, no_nodes_per_dc = 3)
